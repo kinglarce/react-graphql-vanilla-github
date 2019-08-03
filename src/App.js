40c5b17a0d25bfe548deed7fd7b85d9c7f68ec01
King Larce Cortes <larce@Kings-MacBook-Pro.local>
2019-08-03 18:28:36 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const TITLE = 'React GraphQL GitHub Client';
+
+const axiosGitHubGraphQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`,
+  },
+});
+
+class App extends Component {
+  state = {
+    path: 'facebook/react'
+  };
+
+  onSubmit = event => {
+    console.log('SUBMIT');
+    event.preventDefault();
+  };
+
+  render() {
+    const { path } = this.state;
+
+    return (
+      <div>
+        <h1>{TITLE}</h1>
+
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="url">
+            Show open issues for https://github.com/
+          </label>
+          <input
+            id="url"
+            type="text"
+            value={path}
+            style={{ width: '300px' }}
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
