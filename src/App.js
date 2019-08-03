a30e771ac83a845d442854041fb326f1cc2d98ca
King Larce Cortes <larce@Kings-MacBook-Pro.local>
2019-08-03 18:52:31 +0900

    Added adding and removing of stars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import Organization from './components/Organization';
-import { getIssuesOfRepository, resolveIssuesQuery } from './utils';
+import { 
+  getIssuesOfRepository,
+  resolveIssuesQuery,
+  addStarToRepository,
+  resolveAddStarMutation,
+  removeStarFromRepository,
+  resolveRemoveStarMutation
+} from './utils';
 
 const TITLE = 'React GraphQL GitHub Client';
 
@@ -39,6 +46,18 @@ class App extends Component {
     this.onFetchFromGitHub(this.state.path, endCursor);
   };
 
+  onStarRepository = (repositoryId, viewerHasStarred) => {
+    if (viewerHasStarred) {
+      removeStarFromRepository(repositoryId).then(mutationResult =>
+        this.setState(resolveRemoveStarMutation(mutationResult)),
+      );
+    } else {
+      addStarToRepository(repositoryId).then(mutationResult =>
+        this.setState(resolveAddStarMutation(mutationResult)),
+      );
+    }
+  };
+
   render() {
     const { path, organization, errors } = this.state;
 
@@ -67,6 +86,7 @@ class App extends Component {
             organization={organization}
             errors={errors}
             onFetchMoreIssues={this.onFetchMoreIssues}
+            onStarRepository={this.onStarRepository}
           />
         ) : (
           <p>No information yet ...</p>
